262262f900efa28f640acf99af252d83552ee8e6
Bingo Bobo <matmazur90@gmail.com>
2018-12-15 13:05:32 +0100

    added option to put addition parameter in url to show name on jsp page and on sysout

--- src/main/java/com/matmazur/springmvcboot/controllers/HomeController.java
+++ src/main/java/com/matmazur/springmvcboot/controllers/HomeController.java
@@ -3,11 +3,22 @@ package com.matmazur.springmvcboot.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public String home(){
+    @RequestMapping(value = "/")
+    public String home(HttpServletRequest req) {
+
+        String username = req.getParameter("username");
+
+        if (username == null) {
+            System.out.println("no username");
+        } else {
+            System.out.println(username);
+            req.setAttribute("name", username);
+        }
         return "home";
     }
 }
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+<h1>Welcome home</h1>
+<p>${requestScope.get("name")}</p>
+</body>
+</html>
