6a24ea007192fba24343b4d850d007352707da8c
Bingo Bobo <matmazur90@gmail.com>
2018-12-15 13:15:56 +0100

    added cookies request, showing all cookies in sysout

--- src/main/java/com/matmazur/springmvcboot/controllers/HomeController.java
+++ src/main/java/com/matmazur/springmvcboot/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.matmazur.springmvcboot.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -19,6 +20,15 @@ public class HomeController {
             System.out.println(username);
             req.setAttribute("name", username);
         }
+
+        Cookie[] cookies = req.getCookies();
+
+        if (cookies != null && cookies.length != 0) {
+            for (Cookie c : cookies) {
+                System.out.println(c.getName() + " - " + c.getValue());
+            }
+        }
+
         return "home";
     }
 }
