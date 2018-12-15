f91e054d33a60ee48906ed7438fb6eedb338409e
Bingo Bobo <matmazur90@gmail.com>
2018-12-15 15:11:39 +0100

    created super simple calculator (adding) servlet

--- src/main/java/com/matmazur/springmvcboot/controllers/HomeController.java
+++ src/main/java/com/matmazur/springmvcboot/controllers/HomeController.java
@@ -1,9 +1,7 @@
 package com.matmazur.springmvcboot.controllers;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,21 +10,11 @@ public class HomeController {
 
     @RequestMapping(value = "/")
     public String home(
-            @RequestParam(name = "username", defaultValue = "Noname", required = false) String username,
-            @RequestHeader(name = "User-Agent") String agent,
-            @RequestHeader HttpHeaders httpHeaders,
-            ModelMap modelMap) {
-
-        modelMap.put("name", username);
-
-        System.out.println(">>>>>>>>>>>>>>HEADERS>>>>>>>>>>>>");
-        for (String s : httpHeaders.toSingleValueMap().keySet()) {
-            System.out.println(s + "  -  " + httpHeaders.get(s));
-        }
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-        System.out.println(agent);
-        System.out.println(username);
+            ModelMap modelMap,
+            @RequestParam(defaultValue = "0") double a,
+            @RequestParam(defaultValue = "0") double b
+    ) {
+        modelMap.put("result", a + b);
         return "home";
     }
 }
--- src/main/webapp/WEB-INF/view/home.jsp
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -1,10 +1,22 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Home</title>
 </head>
 <body>
-<h1>Welcome home</h1>
-<p>${requestScope.get("name")}</p>
+<h1>Hello home!</h1>
+<h3>Calculator test</h3>
+<form method="post" action="/">
+    <input name="a" type="number">
+    <input name="b" type="number">
+    <input type="submit" value="add numbers">
+
+    <c:set var="result" scope="request" value="${requestScope.get('result')}"/>
+    <c:if test="${result gt 0}">
+        <p><c:out value="${result}"/></p>
+    </c:if>
+
+</form>
 </body>
 </html>
